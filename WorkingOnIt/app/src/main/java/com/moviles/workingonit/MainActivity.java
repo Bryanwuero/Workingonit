af6076ee3b9478621dc066a87f5c4b22c46aa989
Bryan Arroyo <arroyo_bryan@hotmail.com>
2016-08-24 03:27:55 -0500

    Aplicacion buena swipe tabs

--- WorkingOnIt/app/src/main/java/com/moviles/workingonit/MainActivity.java
+++ WorkingOnIt/app/src/main/java/com/moviles/workingonit/MainActivity.java
@@ -126,8 +126,8 @@ public class MainActivity extends AppCompatActivity {
             }
             else{
                 View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-                //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-               // textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
                 return rootView;
             }
         }
